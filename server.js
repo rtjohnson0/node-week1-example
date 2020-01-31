d3e708c4a0d0044be3576281f829f35e09ba7496
Reginald Johnson <regjohnson@c02yj3kljg5j.redventures.net>
2020-01-31 11:13:29 -0500

    adding new methods

--- server.js
+++ server.js
@@ -6,19 +6,67 @@ const app = express();
 app.use(express.json())
 
 
-let student = {
+let student = [ { 
+    id:1,
     name:"reggie",
     age:22,
     school:"NPHS"
-}
+},{
+    id:2,
+    name:"Josh",
+    age:20,
+    school:"WHS"
+
+}]
 
 app.get('/api/test',function(req,res){
    
 res.send(student)
 })
-app.post('/api/test',function(req,res){
-    let data = req.body
+
+app.get('/api/:id', function(req,res){
+    const data = student.find(function(employee){
+        return parseInt(req.params.id) === employee.id
+    })
     res.send(data)
+})
+app.post('/api/test',function(req,res){
+    let data = {
+       name: req.body.name,
+       age: req.body.age,
+       school:req.body.school
+    }
+    if(!data){
+        res.send("Couldnt add employee to list")
+    }
+    student.push(data)
+    res.send(data)
+})
+
+app.put('/api/:id', function(req,res){
+    const data = student.find(function(employee){
+        return parseInt(req.params.id) === employee.id
+    })
+    if(!data){
+        res.send("Couldnt add employee to list")
+    }
+    data.name = req.body.name
+    data.age = req.body.age
+    data.school = req.body.school
+
+    res.send(data)
+})
+
+app.delete('/api/:id', function(req,res){
+    const data = student.find(function(employee){
+        return parseInt(req.params.id) === employee.id
+    })
+
+    const index = student.indexOf(data)
+    student.splice(index,1)
+
+    res.send(data)
+
 })
 
 
